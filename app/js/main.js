67172267d362ddd507440d5cf3b8c55741f7306e
ReflexTen-dev <bulanov.roman4@gmail.com>
2022-09-07 21:51:25 +0500

    added js code

--- app/js/main.js
+++ app/js/main.js
@@ -1,29 +1,28 @@
 'use strict';
 
 const addForm = document.querySelector('.todolist__form'),
+  // boxWrapper = document.querySelector('.todolist__box-wrapper'),
+  // todoButton = document.querySelector('.todolist__button'),
   addInput = document.querySelector('.todolist__input'),
   sumItem = document.querySelector('.footer__num span'),
   toDoList = document.querySelector('.list'),
   delCompleted = document.querySelector('#delete-completed'),
-  activeTask = document.querySelector('#active-task'),
-  allTask = document.querySelector('#all-task'),
+  showActive = document.querySelector('#show-active'),
+  showAll = document.querySelector('#show-all'),
   showCompleted = document.querySelector('#show-completed');
 
 let counter = 0,
-  numTask = 0,
-  i = 0;
+  numTask = 0;
+
 
 addForm.addEventListener('submit', (e) => {
   e.preventDefault();
-
   let task = addInput.value;
-
   if (task) {
-    if (task.length > 21) {
+    if (task.length > 50) {
       task = `${task.substring(0,22)}...`;
     }
   }
-  // console.log(task);
   e.target.reset();
   newToDo(task);
 });
@@ -33,31 +32,58 @@ function quantityChange(amount) {
     ++counter;
   } else if (amount === 'delete') {
     --counter;
+    --numTask;
   }
   sumItem.innerHTML = `${counter}`;
-
 }
 
-function newToDo(newTask) {
+function creatElement(id, value, status, checked) {
+
   let li = document.createElement('li');
   li.classList.add('list__item');
-  numTask++;
-  // console.log(numTask);
 
   li.innerHTML = `
-    <input class="list__checkbox" type="checkbox" name="item-checkbox${numTask}" id="item-checkbox${numTask}">
-
-    <label class="list__label" for="item-checkbox${numTask}">${newTask}</label>
-
+    <input class="list__checkbox" type="checkbox" name="item-checkbox${id}" id="item-checkbox${id}" ${checked}>
+    <label class="list__label ${status}" for="item-checkbox${id}">${value}</label>
       <div class="list__basket">
          <span class="sr-only">Cash</span>
       </div>
   `;
 
   toDoList.append(li);
-
   quantityChange('add');
 }
+
+function newToDo(newTask) {
+  numTask++;
+  creatElement(numTask, newTask, 'not-completed', '');
+
+  let i = {
+    id: numTask,
+    value: newTask,
+    checked: false
+  }
+  localStorage.setItem(`task-${numTask}`, JSON.stringify(i));
+}
+
+
+let listChange = (classHide) => e => {
+  document.querySelectorAll('.active').forEach(item => {
+    item.classList.remove('active');
+  });
+  e.target.classList.add('active');
+
+  document.querySelectorAll('.list__label').forEach(element => {
+    let li = element.closest('.list__item');
+
+    if (element.classList.contains(classHide)) {
+      li.classList.remove('hide');
+    } else {
+      li.classList.add('hide');
+    }
+  });
+};
+
 
 toDoList.addEventListener('click', (e) => {
   let target = e.target;
@@ -65,53 +91,109 @@ toDoList.addEventListener('click', (e) => {
   if (target.classList.contains('list__basket')) {
     let li = target.closest('.list__item');
     li.remove();
-
     quantityChange('delete');
-  }
 
-  if (target.classList.contains('list__label')) {
+    let delNumTask = target.previousElementSibling.getAttribute('for').slice(13);
+    localStorage.removeItem(`task-${delNumTask}`);
+
+  } else if (target.classList.contains('list__label')) {
+
     target.classList.toggle('completed');
-  }
+    target.classList.toggle('not-completed');
 
-  // console.log(e.target);
+    let num = target.getAttribute('for').slice(13);
+
+    let valueTask = localStorage.getItem(`task-${num}`),
+      newValueTask = JSON.parse(valueTask);
+
+    if (newValueTask['checked'] == true) {
+      newValueTask['checked'] = false;
+    } else {
+      newValueTask['checked'] = true;
+    }
+    localStorage.setItem(`task-${num}`, JSON.stringify(newValueTask));
+  }
 });
 
-delCompleted.addEventListener('click', () => {
-  let taskCompleted = document.querySelectorAll('.completed');
 
-  taskCompleted.forEach(element => {
+
+delCompleted.addEventListener('click', () => {
+  document.querySelectorAll('.completed').forEach(element => {
     let li = element.closest('.list__item');
     li.remove();
     quantityChange('delete');
+
+    let num = element.getAttribute('for').slice(13);
+    localStorage.removeItem(`task-${num}`);
   });
 });
 
 
-// showCompleted.addEventListener('click', () => {
-//   let taskCompleted = document.querySelectorAll('.completed');
+showActive.addEventListener('click', listChange('not-completed'));
 
-//   taskCompleted.forEach(element => {
-//     let li = element.closest('.list__item');
-//     li.remove();
-//     quantityChange('delete');
-//   });
-// });
+showCompleted.addEventListener('click', listChange('completed'));
 
-activeTask.addEventListener('click', (e) => {
-  e.target.classList.toggle('active');
-  let taskCompleted = document.querySelectorAll('.completed');
-
-  taskCompleted.forEach(element => {
+showAll.addEventListener('click', () => {
+  document.querySelectorAll('.list__label').forEach(element => {
     let li = element.closest('.list__item');
-    li.style.display = 'none';
+    li.classList.remove('hide');
+  });
+
+  document.querySelectorAll('.active').forEach(item => {
+    item.classList.remove('active');
   });
 });
 
-allTask.addEventListener('click', () => {
-  let taskCompleted = document.querySelectorAll('.completed');
 
-  taskCompleted.forEach(element => {
-    let li = element.closest('.list__item');
-    li.style.display = 'block';
-  });
-});
+function addTaskFromLocalStorage() {
+
+  let counter = 0;
+  for (let i = 0; i < localStorage.length; i++) {
+    let keyNumber = Number(localStorage.key(i).slice(5));
+    if (counter < keyNumber) {
+      counter = keyNumber;
+    }
+  }
+
+  for (let i = 1; i <= counter; i++) {
+    if (!localStorage.getItem(`task-${i}`)) {
+      continue;
+    }
+
+    let task = localStorage.getItem(`task-${i}`),
+      taskValue = JSON.parse(task),
+      status,
+      checked;
+
+    if (taskValue.checked == true) {
+      status = 'completed';
+      checked = 'checked';
+    } else {
+      status = 'not-completed';
+      checked = '';
+    }
+
+    numTask++;
+
+    creatElement(taskValue.id, taskValue.value, status, checked);
+
+    // let li = document.createElement('li');
+    // li.classList.add('list__item');
+
+
+    // li.innerHTML = `
+    //   <input class="list__checkbox" type="checkbox" name="item-checkbox${taskValue.id}" id="item-checkbox${taskValue.id}">
+
+    //   <label class="list__label not-completed" for="item-checkbox${taskValue.id}">${taskValue.value}</label>
+
+    //     <div class="list__basket">
+    //        <span class="sr-only">Cash</span>
+    //     </div>
+    // `;
+
+    // toDoList.append(li);
+    // quantityChange('add');
+  }
+}
+
+addTaskFromLocalStorage();
